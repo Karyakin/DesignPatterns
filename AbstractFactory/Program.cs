ba3a433e5296a578a077ae51bea1d3e6449ef7c6
Дмитрий Карякин <375447955552dk@gmail.com>
2022-09-13 15:27:20 +0300

    fix: fixed

---
+++ AbstractFactory/Program.cs
@@ -0,0 +1,28 @@
+﻿using AbstractFactory;
+using AbstractFactory.Cars;
+using AbstractFactory.Cars.Сoncretely;
+using AbstractFactory.Сoncretely.CocaCola;
+using AbstractFactory.Сoncretely.Pepsi;
+using AbstractFactory.Сoncretely.Schweppes;
+
+try
+{
+    Creator? creator = null;
+    creator = new Creator(new PepsiFactory());
+    creator.Run();
+
+    creator = new Creator(new CocaColaFactory());
+    creator.Run();
+
+    creator = new Creator(new SchweppesFactory());
+    creator.Run();
+
+    CarCreator? carCreator = null;
+    carCreator = new CarCreator(new OpelFactory());
+    carCreator.Run();
+
+}
+catch (Exception e)
+{
+    Console.WriteLine(e.Message);
+}
