02e45c4519bffc4bb2f8a1dc5e59e287d371a9fb
Дмитрий Карякин <375447955552dk@gmail.com>
2022-09-13 18:20:57 +0300

    feat: CountryAbstractFactory

---
+++ AbstractFactory/Cars/Сoncretely/BmwFactory.cs
@@ -0,0 +1,11 @@
+﻿using AbstractFactory.Cars.CarsAbstract;
+using AbstractFactory.Cars.Сoncretely.Bmw;
+
+namespace AbstractFactory.Cars.Сoncretely;
+
+public class BmwFactory : AbstractCarFactory
+{
+    public override AbstractBody CreateBody() => new BmwBody();
+    public override AbstractWheels CreateWheels() => new BmwWheels();
+    public override AbstractEngin CreateEngine() => new BmwEngin();
+}
